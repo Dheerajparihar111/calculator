64c22b66337ad99d22041efc240a4e5854a73f8e
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-01-24 19:43:57 +0530

    added exit option in calculator to exit after you operation are completed

--- src/Main.java
+++ src/Main.java
@@ -2,56 +2,59 @@ import java.util.Scanner;
 
 class home{
     void home(){
-        System.out.println("Enter Your Choice");
-        System.out.println("+");
-        System.out.println("-");
-        System.out.println("/");
-        System.out.println("*");
-        System.out.println("%");
-        System.out.println("EXIT");
+        System.out.println("Enter Your Choice In Integer Please");
+        System.out.println("1) +");
+        System.out.println("2) -");
+        System.out.println("3) *");
+        System.out.println("4) /");
+        System.out.println("5) %");
     }
 }
 
 public class Main{
     static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         home homePage = new home();
+        System.out.println("*************Wel-Come to Calculator************");
         while (true) {
-            System.out.println("*************Wel-Come to Calculator************");
+            System.out.println("Enter The First Value");
+            int number1 = input.nextInt();
             homePage.home();
-            char operator = sc.next().charAt(0);
-            if (operator == 'e'){
-                System.out.println("Thank You For Using Calculator");
-                break;
-            }
-            System.out.println("Enter The First Number");
-            int number1 = sc.nextInt();
+            int operator = input.nextInt();
             System.out.println("Enter the Second  Number");
-            int number2 = sc.nextInt();
+            int number2 = input.nextInt();
 
             switch (operator) {
-                case '+':
+                case 1:
                     System.out.println("Result =" + (number1 + number2));
                     break;
-                case '-':
+                case 2:
                     System.out.println("Result =" + (number1 - number2));
                     break;
-                case '*':
+                case 3:
                     System.out.println("Result =" + (number1 * number2));
                     break;
-                case '/':
+                case 4:
                     if (number2 == 0) {
-                        System.out.println("Invalid!!");
+                        System.out.println("Invalid!! Cannot Divide By Zero ");
                     }
                     else {
                         System.out.println("Result =" + (number1 / number2));
                     }
                     break;
-                case '%':
+                case 5:
                     System.out.println("Result =" + (number1 % number2));
                     break;
 
-
+            }
+            System.out.println("Do You Want To Exit Calculator");
+            System.out.println(" 1) Yes OR 2) No");
+            int quitOption = input.nextInt();
+            if (quitOption == 1){
+                System.out.println("Thank You For Using Calculator");
+                System.exit(0);
+            }else {
+                continue;
             }
         }
 
